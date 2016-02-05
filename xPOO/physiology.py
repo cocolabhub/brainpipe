849ab420f78b5377c82a79fb33e4d45521665ba4
EtienneCmb <e.combrisson@gmail.com>
2016-02-05 00:50:35 -0500

    New class for managing physiological informations

--- xPOO/physiology.py
+++ xPOO/physiology.py
@@ -1,34 +1,43 @@
 import numpy as n
-import pandas as pd
+from pandas import DataFrame
 from brainpipe.xPOO._utils._physio import loadatlas, pos2label
+from brainpipe.xPOO._utils._dataframe import *
 
 
-class physio():
+class physio(object):
 
-    def __init__(self, atlas='tal', r=5, nearest=True):
+    def __init__(self, atlas='tal', r=5, nearest=True, rm_unfound=False):
         self.atlas = atlas
         self.r = r
         self.nearest = nearest
-        (self.__hdr, self.__mask, self.__gray, self.__brodtxt,
-            self.__brodidx, self.__label) = loadatlas(atlas=atlas)
+        self.rm_unfound = rm_unfound
+        (self.__hdr, self.__mask, self.__gray,
+            self.__label) = loadatlas(atlas=self.atlas, r=self.r)
 
     def get(self, xyz, channel=''):
-        print('')
+        pdPhy = pos2label(xyz, self.__mask, self.__hdr, self.__gray,
+                          self.__label, r=self.r, nearest=self.nearest)
+        return pd2physio(pdPhy)
 
 
-class pd2physio(pd.Dataframe):
+class pd2physio(DataFrame):
 
     def __new__(self, x):
-        return pd.Dataframe.__new__(self, x)
+        return DataFrame.__new__(self)
 
-    def keep():
+    def search(self, *arg):
+        return pdSearch(self, *arg)
+
+    def keep(self, *arg, keep_idx=True):
+        pdFrame, _ = pdKeep(self, *arg, keep_idx=keep_idx)
+        return pd2physio(pdFrame)
+
+    def rm(self, *arg, rm_idx=True):
+        pdFrame, _ = pdRm(self, *arg, rm_idx=rm_idx)
+        return pd2physio(pdFrame)
+
+    def order(self):
         print('')
 
-    def rm():
-        print('')
-
-    def order():
-        print('')
-
-    def plot():
+    def plot(self):
         print('')
