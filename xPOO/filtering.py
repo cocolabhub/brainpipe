2d2b6de495539a2b6f17c5818ed85a87cf6f26f1
EtienneCmb <e.combrisson@gmail.com>
2016-02-12 17:50:12 -0500

    Cleaning the filtering module

--- xPOO/filtering.py
+++ xPOO/filtering.py
@@ -3,8 +3,11 @@ Design a filter, filt a signal, extract the phase, amplitude or power
 """
 
 import numpy as n
-from scipy.signal import filtfilt, butter, bessel, hilbert, hilbert2, detrend
-from brainpipe.xPOO._utils._filtering import fir_order, fir1, morlet
+from brainpipe.xPOO._utils._filtering import (fir_order,
+                                              fir1,
+                                              morlet,
+                                              _get_method,
+                                              _apply_method)
 
 __all__ = [
     'fdesign',
@@ -56,30 +59,6 @@ class fdesign(object):
                 self.order)+', axis='+str(self.axis)+')'
         return filtStr
 
-    def _getFiltDesign(self, sf, f, npts):
-        """Get the designed filter
-        sf : sample frequency
-        f : frequency vector/list [ex : f = [2,4]]
-        npts : number of points
-        """
-        if type(f) != n.ndarray:
-            f = n.array(f)
-        if self.filtname == 'fir1':
-            fOrder = fir_order(sf, npts, f[0], cycle=self.cycle)
-            b, a = fir1(fOrder, f/(sf / 2))
-        elif self.filtname == 'butter':
-            b, a = butter(self.order, [(2*f[0])/sf,
-                                       (2*f[1])/sf], btype='bandpass')
-            fOrder = None
-        elif self.filtname == 'bessel':
-            b, a = bessel(self.order, [(2*f[0])/sf,
-                                       (2*f[1])/sf], btype='bandpass')
-            fOrder = None
-
-        def filSignal(x): return filtfilt(b, a, x, padlen=fOrder,
-                                          axis=self.axis)
-        return filSignal
-
 
 class fextract(fdesign):
 
@@ -130,14 +109,14 @@ class fextract(fdesign):
 
     Method
     ----------
-    getMeth : get the list of methods
+    get : get the list of methods
         sf : sample frequency
         f : frequency vector/list [ex : f = [ [2,4], [5,7], [8,13] ]]
         npts : number of points
     -> Return a list of methods. The length of the list depend on the
     length of the frequency list "f".
 
-    applyMeth : apply the list of methods
+    apply : apply the list of methods
         x : array signal, [x] = npts x ntrials
         fMeth : list of methods
     -> Return a 3D array nFrequency x npts x ntrials
@@ -178,48 +157,7 @@ class fextract(fdesign):
         return 'Extract(kind='+self.kind+', method='+self.method+', detrend='+str(
             self.dtrd)+supStr+',\n'+filtStr+')'
 
-    def _getTransform(self, sf, f, npts):
-
-        fDesign = self._getFiltDesign(sf, f, npts)
-
-        if self.method == 'hilbert':      # Hilbert method
-            def hilb(x):
-                xH = n.zeros(x.shape)*1j
-                xF = fDesign(x)
-                for k in range(0, x.shape[1]):
-                    xH[:, k] = hilbert(xF[:, k])
-                return xH
-            return hilb
-        elif self.method == 'hilbert1':   # Hilbert method 1
-            def hilb1(x): return hilbert(fDesign(x))
-            return hilb1
-        elif self.method == 'hilbert2':   # Hilbert method 2
-            def hilb2(x): return hilbert2(fDesign(x))
-            return hilb2
-        elif self.method == 'wavelet':    # Wavelet method
-            def wav(x): return morlet(x, sf, (f[0]+f[1])/2,
-                                      wavelet_width=self.wltWidth)
-            return wav
-        elif self.method == 'filter':     # Filter the signal
-            def fm(x): return fDesign(x)
-            return fm
-
-    def _getKind(self):
-
-        if self.kind == 'signal':        # Unmodified signal
-            def sig_k(x): return x
-            return sig_k
-        elif self.kind == 'phase':       # phase of the filtered signal
-            def phase_k(x): return n.angle(x)
-            return phase_k
-        elif self.kind == 'amplitude':   # amplitude of the filtered signal
-            def amp_k(x): return abs(x)
-            return amp_k
-        elif self.kind == 'power':       # power of the filtered signal
-            def pow_k(x): return n.square(abs(x))
-            return pow_k
-
-    def getMeth(self, sf, f, npts):
+    def get(self, sf, f, npts):
         """Get the methods
         sf : sample frequency
         f : frequency vector/list [ ex : f = [[2,4],[5,7]] ]
@@ -228,36 +166,15 @@ class fextract(fdesign):
         """
         if type(f[0]) == int:
             f = [f]
-        xKind = self._getKind()
-        fmeth = []
-        for k in f:
-            def fme(x, fce=k): return xKind(self._getTransform(
-                sf, fce, npts)(x))
-            fmeth.append(fme)
-        return fmeth
+        fMeth = _get_method(sf, f, npts, self.filtname, self.cycle, self.order,
+                            self.axis, self.method, self.wltWidth, self.kind)
+        return fMeth
 
-    def applyMeth(self, x, fMeth):
+    def apply(self, x, fMeth):
         """Apply the methods
         x : array signal
         fMeth : list of methods
         -> 3D array of the transform signal
         """
-        npts, ntrial = x.shape
-        nFce = len(fMeth)
-        xf = n.zeros((nFce, npts, ntrial))
-
-        # Detrend the signal :
-        if self.dtrd:
-            x = detrend(x, axis=0)
-
-        # Apply methods :
-        for k in range(0, nFce):  # For each frequency in the tuple
-            xf[k, ...] = fMeth[k](x)
-
-        # Correction for the wavelet (due to the wavelet width):
-        if (self.method == 'wavelet') and (self.wltCorr is not None):
-            w = 3*self.wltWidth
-            xf[:, 0:w, :] = xf[:, w+1:2*w+1, :]
-            xf[:, npts-w:npts, :] = xf[:, npts-2*w-1:npts-w-1, :]
-
-        return xf
+        return _apply_method(x, fMeth, self.dtrd, self.method,
+                             self.wltCorr, self.wltWidth)
