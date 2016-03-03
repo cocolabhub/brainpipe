4c8fda1b8e686e67a189d13a92abf7c58422589e
EtienneCmb <e.combrisson@gmail.com>
2016-03-03 11:04:44 -0500

    Add mulitifeatures with groups combinations

---
+++ xPOO/_utils/_sequence.py
@@ -0,0 +1,121 @@
+import numpy as n
+from joblib import Parallel, delayed
+from itertools import combinations
+
+__all__ = ['_sequence']
+
+
+def _sequence(x, y, clfObj, direction, grp, n_jobs, display, cwi):
+    """Run the inner sequence forward/backward/exhaustiv.
+    """
+    # Check inputs size :
+    y = n.ravel(y)
+    grpwrd = 'group'
+    if x.shape[0] != len(y):
+        x = x.T
+    if not isinstance(grp, list):
+        grp = list(grp)
+    if not grp:
+        grp = n.arange(x.shape[1])
+        grpwrd = 'feature'
+    grp = {'g'+str(l): [i for i, j in enumerate(grp) if j == k]
+           for l, k in enumerate(set(grp))}
+    grpu = [k for k in range(len(set(grp)))]
+    nfeat = len(grpu)
+    ntrial, _ = x.shape
+
+    # Initialize list :
+    if direction == 'forward':
+        cst, dyn = [], grpu.copy()
+        dirwrd = 'added'
+        new_score = 0.05
+    elif direction == 'backward':
+        cst, dyn = grpu.copy(), grpu.copy()
+        dirwrd = 'removed'
+        new_score = clfObj.fit(x, n_jobs=1, mf=True)[0][0]
+    elif direction == 'exhaustive':
+        cst, dyn = grpu.copy(), []
+        new_score = 0.05
+    combi = _seqcombination(cst, dyn, direction, grp)
+
+    if display:
+        print('-> Run the '+direction+' feature selection')
+
+    # 2 - Classify all features to find the best one :
+    old_score = 0
+    k = 0
+    flist = []
+    while k <= nfeat - 1 and old_score <= new_score:
+        # Classify all combinations :
+        old_score = new_score
+        new_score, indu, all_scores = clfcombi(clfObj, x, combi, n_jobs=n_jobs)
+        ind = combi[indu]
+
+        # Update variables:
+        if direction == 'forward':
+            igrp = grpu[indu]
+            cst.append(igrp)
+            grpu = n.delete(grpu, indu)
+            dyn = grpu.copy()
+            flist = cst[0:-1]
+        elif direction == 'backward':
+            flist = cst
+            igrp = grpu[indu]
+            grpu = n.delete(grpu, indu)
+            cst = grpu.copy()
+            dyn = grpu.copy()
+        elif direction == 'exhaustive':
+            flist = ind
+            k = nfeat + 1
+
+        if direction != 'exhaustive':
+            k += 1
+            if display:
+                print('Step', k, '- ', grpwrd, dirwrd,
+                      ':', igrp, '|| DA =', new_score)
+            combi = _seqcombination(cst, dyn, direction, grp)
+
+            if cwi:
+                old_score = new_score
+            flist = [j for k in flist for j in grp['g'+str(k)]]
+
+    return flist
+
+
+def _seqcombination(cst, dyn, direction, grp):
+    """Generate combi for forward/backward/exhaustive sequence.
+
+    cst : list containing the index of all the features
+    dyn : features to add or remove
+    direction : direction of the sequence
+    grp : group features
+    """
+    if direction == 'forward':
+        combi = [cst + [y]
+                 for y in dyn if not list(set(cst).intersection([y]))]
+    elif direction == 'backward':
+        combi = [list(set(cst).difference([x])) for x in dyn]
+    elif direction == 'exhaustive':
+        combi = [list(k) for i in range(1, len(cst)+1)
+                 for k in combinations(cst, i)]
+
+    return [[j for i in k for j in grp['g'+str(i)]] for k in combi]
+
+
+def clfcombi(clfObj, x, combi, n_jobs=1):
+    """Classify each combi and return scores and the best one location.
+    """
+    # - Classify each combination :
+    score = n.array(Parallel(n_jobs=n_jobs)(
+        delayed(_clfcombi)(clfObj, x[..., k]) for k in combi))
+
+    return score.max(), score.argmax(), score
+
+
+def _clfcombi(clfObj, x):
+    """Classify one combination
+    """
+    if x.shape[1] != 0:
+        return n.mean(clfObj.fit(x, n_jobs=1, mf=True))
+    else:
+        return 0
---
+++ xPOO/mf/methods.py
@@ -0,0 +1,114 @@
+import numpy as n
+
+from brainpipe.xPOO._utils._sequence import *
+
+__all__ = ['Id2methods', 'applyMethods']
+
+
+####################################################################
+# - Select and return a mf model :
+####################################################################
+
+def Id2methods(Id, y, clf, p=0.05, display=False, direction='forward',
+               nbest=10, stat='bino', n_perm=200):
+    """Transform a Id to a list of methods
+
+    '0': No selection. All the features are used
+    '1': Select <p significant features using either a binomial law
+         or permutations
+    '2': select 'nbest' features
+    '3': use 'forward'/'backward'/'exhaustive' to  select features
+    """
+
+    # Define all the methods :
+    def submeth(Idx):
+
+        # Select all features
+        if Idx == '0':
+            def MFmeth(x, GRP):
+                return select_all(x)
+            StrMeth = 'SelectAll'
+
+        # use a binomial law to select features
+        if Idx == '1':
+            def MFmeth(x, GRP):
+                return select_stat(x, y, clf, stat, GRP, p, n_perm)
+
+            if stat == 'bino':
+                StrMeth = 'Binomial selection at p<'+str(p)
+            elif stat.lower().find('_rnd')+1:
+                StrMeth = 'Permutations ('+stat+') selection at p<'+str(p)
+
+        # nbest features
+        if Idx == '2':
+            def MFmeth(x, GRP):
+                return select_nbest(x, y, clf, nbest, GRP)
+            StrMeth = str(nbest)+' best features'
+
+        # use 'forward'/'backward'/'exhaustive'to  select features
+        if Idx == '3':
+
+            def MFmeth(x, GRP):
+                return _sequence(x, y, clf, direction, GRP, 1, display, False)
+            StrMeth = direction+' feature selection'
+
+        return MFmeth, StrMeth
+
+    # Define a list containing the methods:
+    return [submeth(k)[0] for k in Id], [submeth(k)[1] for k in Id]
+
+
+####################################################################
+# - Apply the selected model to the dataset :
+####################################################################
+
+
+def applyMethods(MFmeth, MFstr, x, grp):
+    """Apply a list of method to the features x. MFstr contain the name of each
+    method"""
+
+    idx = list(n.arange(x.shape[1]))
+
+    # Each method find features and return a reduce set:
+    def findAndSelect(meth, x, grp, idxO):
+        idxN = meth(x, grp)
+        return x[:, idxN], [grp[k] for k in idxN], [idxO[k] for k in idxN]
+
+    # Apply each method and get the final set of features:
+    for k in MFmeth:
+        if not x.size or not idx:
+            break
+        x, grp, idx = findAndSelect(k, x, grp, idx)
+
+    # String of method application:
+    MFstrCascade = ' => '.join(MFstr)
+
+    return x, idx, grp, MFstrCascade
+
+
+####################################################################
+# - MF methods :
+####################################################################
+
+
+def select_all(x):
+    """Select and return idx of every features"""
+    return list(n.arange(x.shape[1]))
+
+
+def select_stat(x, y, clf, meth, grp, p, n_perm):
+    """Select and return idx of significant features"""
+    _, pvalue, _ = clf.fit_stat(x, mf=True, grp=grp, method=meth,
+                                n_perm=n_perm, n_jobs=1)
+    return grp2idx(grp, [k for k, i in enumerate(pvalue) if i <= p])
+
+
+def select_nbest(x, y, clf, nbest, grp):
+    """Select nbest features"""
+    da = n.mean(clf.fit(x, mf=True, grp=grp, n_jobs=1), 1)
+    return grp2idx(grp, list(n.ravel(da.T).argsort()[-nbest:][::-1]))
+
+
+def grp2idx(grp, idx):
+    """This function return the index of a group list"""
+    return [k for k, i in enumerate(grp) for j, l in enumerate(idx) if i == l]
